3a2b78b2e6270b6ff5de9c44be29b56ec9afb477
Penang Teams <raj_ppd@yahoo.com>
2024-10-20 17:13:54 +0800

    active navbar done

--- src/app/components/navbar/main-nav.tsx
+++ src/app/components/navbar/main-nav.tsx
@@ -1,19 +1,49 @@
 /** @format */
-
+"use client";
 import { Apple } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function MainNav() {
+  const pathname = usePathname();
   return (
     <div className="hidden md:flex">
       <Link href="/">
         <Apple className="text-red-500" fill="#97d717" size={46} />
       </Link>
       <nav className="flex items-center gap-3 lg:gap-4 ml-8 ">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/admin">Dashboard</Link>
+        <Link
+          className={`[&.active]:bg-orange-100 [&.active]:text-orange-600 [&.active]:rounded [&.active]:px-2 [&.active]:py-2 ${
+            pathname === "/" ? "active" : ""
+          }`}
+          href="/"
+        >
+          Home
+        </Link>
+        <Link
+          className={`[&.active]:bg-orange-100 [&.active]:text-orange-600 [&.active]:rounded [&.active]:px-2 [&.active]:py-2 ${
+            pathname === "/about" ? "active" : ""
+          }`}
+          href="/about"
+        >
+          About
+        </Link>
+        <Link
+          className={`[&.active]:bg-orange-100 [&.active]:text-orange-600 [&.active]:rounded [&.active]:px-2 [&.active]:py-2 ${
+            pathname === "/contact" ? "active" : ""
+          }`}
+          href="/contact"
+        >
+          Contact
+        </Link>
+        <Link
+          className={`[&.active]:bg-orange-100 [&.active]:text-orange-600 [&.active]:rounded [&.active]:px-2 [&.active]:py-2 ${
+            pathname === "/admin" ? "active" : ""
+          }`}
+          href="/admin"
+        >
+          Dashboard
+        </Link>
       </nav>
     </div>
   );
--- src/app/components/navbar/mobile-nav.tsx
+++ src/app/components/navbar/mobile-nav.tsx
@@ -1,5 +1,5 @@
 /** @format */
-
+"use client";
 import {
   Sheet,
   SheetContent,
@@ -10,8 +10,10 @@ import {
 } from "@/app/components/ui/sheet";
 import { AlignJustify, Apple } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function MobileNav() {
+  const pathname = usePathname();
   return (
     <div className="md:hidden">
       <Sheet>
@@ -28,10 +30,39 @@ export default function MobileNav() {
                 <Apple className="text-red-500" fill="#97d717" size={46} />
               </Link>
               <nav className="flex flex-col gap-3 lg:gap-4 mt-6">
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
-                <Link href="/contact">Contact</Link>
-                <Link href="/admin">Dashboard</Link>
+                <Link
+                  className={`px-2 py-2 [&.active]:bg-orange-100 [&.active]:text-orange-600 [&.active]:rounded [&.active]:px-2 [&.active]:py-2 ${
+                    pathname === "/" ? "active" : ""
+                  }`}
+                  href="/"
+                >
+                  {" "}
+                  Home
+                </Link>
+                <Link
+                  className={`px-2 py-2 [&.active]:bg-orange-100 [&.active]:text-orange-600 [&.active]:rounded [&.active]:px-2 [&.active]:py-2 ${
+                    pathname === "/about" ? "active" : ""
+                  }`}
+                  href="/about"
+                >
+                  About
+                </Link>
+                <Link
+                  className={`px-2 py-2 [&.active]:bg-orange-100 [&.active]:text-orange-600 [&.active]:rounded [&.active]:px-2 [&.active]:py-2 ${
+                    pathname === "/contact" ? "active" : ""
+                  }`}
+                  href="/contact"
+                >
+                  Contact
+                </Link>
+                <Link
+                  className={`px-2 py-2 [&.active]:bg-orange-100 [&.active]:text-orange-600 [&.active]:rounded [&.active]:px-2 [&.active]:py-2 ${
+                    pathname === "/admin" ? "active" : ""
+                  }`}
+                  href="/admin"
+                >
+                  Dashboard
+                </Link>
               </nav>
             </SheetContent>
           </SheetDescription>
