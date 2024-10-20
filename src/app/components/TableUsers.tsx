1ebcd2039327bae549a9756a13272000a9a6518c
Penang Teams <raj_ppd@yahoo.com>
2024-10-20 22:00:25 +0800

    admin page search on table enabled

--- src/app/components/TableUsers.tsx
+++ src/app/components/TableUsers.tsx
@@ -1,4 +1,5 @@
 /** @format */
+"use client";
 import {
   Table,
   TableBody,
@@ -8,8 +9,21 @@ import {
   TableHeader,
   TableRow,
 } from "@/app/components/ui/table";
+import Link from "next/link";
+import { c_data } from "./clientdata";
+import { useSearchStore } from "@/contexts/store";
 
 export const TableUsers = () => {
+  const search = useSearchStore((state) => state.searchTerm);
+  const filteredList = c_data.filter(
+    (item) =>
+      item.verdict.toLowerCase().includes(search.toLowerCase()) ||
+      item.username.toLowerCase().includes(search.toLowerCase()) ||
+      item.place.toLowerCase().includes(search.toLowerCase()) ||
+      item.description.toLowerCase().includes(search.toLowerCase()) ||
+      item.age.toString().toLowerCase().includes(search.toLowerCase()) ||
+      item.amount.toString().toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <>
       <Table>
@@ -26,38 +40,30 @@ export const TableUsers = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">sunny_girl</TableCell>
-            <TableCell>28</TableCell>
-            <TableCell>Miami</TableCell>
-            <TableCell>
-              Will I get to the University admission for my desired Graduate
-              Program?
-            </TableCell>
-            <TableCell>Yes, it is on your way</TableCell>
-            <TableCell className="text-right">$20.00</TableCell>
-            <TableCell className="text-right">Action</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">harryboy</TableCell>
-            <TableCell>33</TableCell>
-            <TableCell>Dubai</TableCell>
-            <TableCell>Will I marry soon with my gf?</TableCell>
-            <TableCell>It will take about 6 months</TableCell>
-            <TableCell className="text-right">$10.00</TableCell>
-            <TableCell className="text-right">Action</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">redhair</TableCell>
-            <TableCell>51</TableCell>
-            <TableCell>Los Angeles</TableCell>
-            <TableCell>Will I re-marry and will it do good for me?</TableCell>
-            <TableCell>
-              Yes you can, but do it end of the year and you will be happy
-            </TableCell>
-            <TableCell className="text-right">$32.00</TableCell>
-            <TableCell className="text-right">Action</TableCell>
-          </TableRow>
+          {filteredList.length === 0 ? (
+            <div>No data</div>
+          ) : (
+            filteredList.map((el, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell className="font-medium">{el.username}</TableCell>
+                  <TableCell>{el.age}</TableCell>
+                  <TableCell>{el.place}</TableCell>
+                  <TableCell>{el.description}</TableCell>
+                  <TableCell>{el.verdict}</TableCell>
+                  <TableCell className="text-right">$ {el.amount}</TableCell>
+                  <TableCell className="text-right">
+                    <Link
+                      href="/"
+                      className="inline-block px-2 py-2 mx-auto text-white bg-pink-600 rounded-full hover:bg-pink-700 md:mx-0 text-xs text-center"
+                    >
+                      Check
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              );
+            })
+          )}
         </TableBody>
       </Table>
     </>
---
+++ src/app/components/clientdata.ts
@@ -0,0 +1,33 @@
+/** @format */
+
+let c_data = [
+  {
+    id: 1,
+    username: "sunny_girl",
+    age: 28,
+    place: "Miami",
+    description:
+      "Will I get to the University admission for my desired Graduate Program?",
+    verdict: "Yes, it is on your way",
+    amount: 20,
+  },
+  {
+    id: 2,
+    username: "harryboy",
+    age: 33,
+    place: "Dubai",
+    description: "Will I marry soon with my gf?",
+    verdict: "It will take about 6 months",
+    amount: 10,
+  },
+  {
+    id: 3,
+    username: "redhair",
+    age: 51,
+    place: "Los Angeles",
+    description: "Will I re-marry and will it do good for me?",
+    verdict: "Yes you can, but do it end of the year and you will be happy",
+    amount: 32,
+  },
+];
+export { c_data };
